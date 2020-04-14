49c9a1af18198afc92904edbfa112eca82a0145b
Castaneda <FCastaneda@de.alten.com>
2020-04-14 14:08:44 +0200

    List and create prototypes

--- config/init.php
+++ config/init.php
@@ -1,6 +1,15 @@
 <?php
+
+//Session start
+session_start();
+
 //Config File
 require_once 'config/config.php';
+
+
+//Include Helper
+require_once 'helpers/system_helper.php';
+
 
 // Autloader
 function __autoload($class_name){
