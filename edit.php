0d3009f62e92d7548855ba997e18c8bba802a46f
Castaneda <FCastaneda@de.alten.com>
2020-04-16 12:31:06 +0200

    Edit functionality

---
+++ edit.php
@@ -0,0 +1,36 @@
+<?php include_once 'config/init.php'; ?>
+
+<?php
+
+$job = new Job;
+
+$job_id = isset($_GET['id']) ? $_GET['id'] : null;
+
+
+if(isset($_POST['submit'])){
+    // Create Data Array
+    $data = array();
+    $data['job_title'] = $_POST['job_title'];
+    $data['company'] = $_POST['company'];
+    $data['category_id'] = $_POST['category'];
+    $data['description'] = $_POST['description'];
+    $data['location'] = $_POST['location'];
+    $data['salary'] = $_POST['salary'];
+    $data['contact_user'] = $_POST['contact_user'];
+    $data['contact_email'] = $_POST['contact_email'];
+
+    if($job->update($job_id, $data)){        
+        redirect('index.php', 'Job aktualisiert', 'success');
+    }else{        
+        redirect('index.php', 'Etwas ist ausgelaufen', 'error');
+    }
+}
+
+$template = new Template('templates/job-edit.php');
+
+$template->categories = $job->getCategories();
+
+$template->job = $job->getJob($job_id);
+
+echo $template;
+?>
--- templates/job-single.php
+++ templates/job-single.php
@@ -17,6 +17,14 @@
 <a href="index.php"> Go Back </a>
 
 <br><br>
+<div class="well">
+    <a href="edit.php?id=<?php echo $job->id; ?>" class="btn btn-secondary">Edit</a>
+
+    <form style="display:inline;" method="post" action="job.php">
+        <input type="hidden" name="del_id" value="<?php echo $job->id; ?>">
+        <input type="submit" class="btn btn-danger" value="Delete">
+    </form>
+</div>
 </div>
 
 <?php include 'inc/footer.php';?>
